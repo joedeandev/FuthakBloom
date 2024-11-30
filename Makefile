69e931053b766e7cf083c37748b02282ce580d7e
Joe Dean <joe@joedean.dev>
2024-12-01 00:00:00 +0100

    feat(futhak-bloom): improve keymap with `zmk-helper`

--- Makefile
+++ Makefile
@@ -5,10 +5,16 @@ BUILD?="build/futhak-bloom"
 DSHIELD?="lily58_left"
 DEST?=".."
 NAME?="futhak-bloom"
+PRISTINE?="--pristine"
 
+west-setup:
+	python -m venv .venv && \
+	source .venv/bin/activate && \
+	west init && \
+	west update
 build-base:
 	cd $(ROOT) && \
-	west build -d $(BUILD) -b $(BOARD) -- -DSHIELD=$(DSHIELD) -DZMK_CONFIG=$(CONFIG) && \
+	west build -d $(BUILD) -b $(BOARD) $(PRISTINE) -- -DSHIELD=$(DSHIELD) -DZMK_CONFIG=$(CONFIG) && \
 	cp $(BUILD)/zephyr/zmk.uf2 $(DEST)/$(NAME).uf2
 build-left:
 	$(MAKE) build-base BUILD=build/left DSHIELD=lily58_left NAME=futhak-bloom-left
---
+++ config/defines.h
@@ -0,0 +1,194 @@
+// ┌───────────────────────────────────────────────────────────────────────────┐
+//                                      KEYS                                    
+// └───────────────────────────────────────────────────────────────────────────┘
+#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4
+#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4
+#define KEYS_T LH2 LH1 LH0 RH0 RH1 RH2
+
+// ┌───────────────────────────────────────────────────────────────────────────┐
+//                                     LAYERS                                   
+// └───────────────────────────────────────────────────────────────────────────┘
+#define L_BASE 0
+#define L_NUMBER 1
+#define L_NAVMOUSE 2
+#define L_CONTROL 3
+#define L_GAME_1 4
+#define L_GAME_2 5
+#define L_GAME_3 6
+#define L_GAME_ALT 7
+#define L_ALTERNATE_1 4
+
+// ┌───────────────────────────────────────────────────────────────────────────┐
+//                                   EXTRA KEYS                                 
+// └───────────────────────────────────────────────────────────────────────────┘
+#if !defined X_UPR0
+    #define X_UPR0
+#endif
+#if !defined X_LTP0
+    #define X_LTP0
+#endif
+#if !defined X_MTP0
+    #define X_MTP0
+#endif
+#if !defined X_RTP0
+    #define X_RTP0
+#endif
+#if !defined X_LMD0
+    #define X_LMD0
+#endif
+#if !defined X_MMD0
+    #define X_MMD0
+#endif
+#if !defined X_RMD0
+    #define X_RMD0
+#endif
+#if !defined X_LBM0
+    #define X_LBM0
+#endif
+#if !defined X_MBM0
+    #define X_MBM0
+#endif
+#if !defined X_RBM0
+    #define X_RBM0
+#endif
+#if !defined X_LTM0
+    #define X_LTM0
+#endif
+#if !defined X_MTM0
+    #define X_MTM0
+#endif
+#if !defined X_RTM0
+    #define X_RTM0
+#endif
+#if !defined X_LWR0
+    #define X_LWR0
+#endif
+
+#if !defined X_UPR1
+    #define X_UPR1 X_UPR0
+#endif
+#if !defined X_LTP1
+    #define X_LTP1 X_LTP0
+#endif
+#if !defined X_MTP1
+    #define X_MTP1 X_MTP0
+#endif
+#if !defined X_RTP1
+    #define X_RTP1 X_RTP0
+#endif
+#if !defined X_LMD1
+    #define X_LMD1 X_LMD0
+#endif
+#if !defined X_MMD1
+    #define X_MMD1 X_MMD0
+#endif
+#if !defined X_RMD1
+    #define X_RMD1 X_RMD0
+#endif
+#if !defined X_LBM1
+    #define X_LBM1 X_LBM0
+#endif
+#if !defined X_MBM1
+    #define X_MBM1 X_MBM0
+#endif
+#if !defined X_RBM1
+    #define X_RBM1 X_RBM0
+#endif
+#if !defined X_LTM1
+    #define X_LTM1 X_LTM0
+#endif
+#if !defined X_MTM1
+    #define X_MTM1 X_MTM0
+#endif
+#if !defined X_RTM1
+    #define X_RTM1 X_RTM0
+#endif
+#if !defined X_LWR1
+    #define X_LWR1 X_LWR0
+#endif
+
+#if !defined X_UPR2
+    #define X_UPR2 X_UPR0
+#endif
+#if !defined X_LTP2
+    #define X_LTP2 X_LTP0
+#endif
+#if !defined X_MTP2
+    #define X_MTP2 X_MTP0
+#endif
+#if !defined X_RTP2
+    #define X_RTP2 X_RTP0
+#endif
+#if !defined X_LMD2
+    #define X_LMD2 X_LMD0
+#endif
+#if !defined X_MMD2
+    #define X_MMD2 X_MMD0
+#endif
+#if !defined X_RMD2
+    #define X_RMD2 X_RMD0
+#endif
+#if !defined X_LBM2
+    #define X_LBM2 X_LBM0
+#endif
+#if !defined X_MBM2
+    #define X_MBM2 X_MBM0
+#endif
+#if !defined X_RBM2
+    #define X_RBM2 X_RBM0
+#endif
+#if !defined X_LTM2
+    #define X_LTM2 X_LTM0
+#endif
+#if !defined X_MTM2
+    #define X_MTM2 X_MTM0
+#endif
+#if !defined X_RTM2
+    #define X_RTM2 X_RTM0
+#endif
+#if !defined X_LWR2
+    #define X_LWR2 X_LWR0
+#endif
+
+#if !defined X_UPR3
+    #define X_UPR3 X_UPR0
+#endif
+#if !defined X_LTP3
+    #define X_LTP3 X_LTP0
+#endif
+#if !defined X_MTP3
+    #define X_MTP3 X_MTP0
+#endif
+#if !defined X_RTP3
+    #define X_RTP3 X_RTP0
+#endif
+#if !defined X_LMD3
+    #define X_LMD3 X_LMD0
+#endif
+#if !defined X_MMD3
+    #define X_MMD3 X_MMD0
+#endif
+#if !defined X_RMD3
+    #define X_RMD3 X_RMD0
+#endif
+#if !defined X_LBM3
+    #define X_LBM3 X_LBM0
+#endif
+#if !defined X_MBM3
+    #define X_MBM3 X_MBM0
+#endif
+#if !defined X_RBM3
+    #define X_RBM3 X_RBM0
+#endif
+#if !defined X_LTM3
+    #define X_LTM3 X_LTM0
+#endif
+#if !defined X_MTM3
+    #define X_MTM3 X_MTM0
+#endif
+#if !defined X_RTM3
+    #define X_RTM3 X_RTM0
+#endif
+#if !defined X_LWR3
+    #define X_LWR3 X_LWR0
+#endif
